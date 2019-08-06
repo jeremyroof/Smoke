c8c46252b42d693152b68e8cae5724b4654603d7
Jeremy Roof <jeremyroof@gmail.com>
2019-08-06 10:58:47 -0400

    Initial Commit

---
+++ Smoke/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Smoke
+//
+//  Created by Jeremy Roof on 8/6/19.
+//  Copyright © 2019 Jeremy Roof. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
